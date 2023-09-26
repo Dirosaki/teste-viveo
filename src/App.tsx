bde322d9af287455fe75207cc3478fb8004973a5
Dirosaki <diegosgomes27@gmail.com>
2023-09-26 14:25:49 -0300

    feat: Add routes and MUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import { RouterProvider } from 'react-router-dom'
+
+import { ThemeProvider, createTheme } from '@mui/material'
+import { routes } from 'Router'
+
 import 'styles/global.css'
 
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+})
+
 export const App = () => {
-  return <h1>Hello World!</h1>
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <RouterProvider router={routes} />
+    </ThemeProvider>
+  )
 }
---
+++ src/Router.tsx
@@ -0,0 +1,5 @@
+import { createBrowserRouter } from 'react-router-dom'
+
+export const routes = createBrowserRouter([
+  { path: '/login', element: <h1>Login</h1> },
+])
