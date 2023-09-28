5c9afcd240a88cbb79cb1bdc9eb65029bf33c7d5
Dirosaki <diegosgomes27@gmail.com>
2023-09-28 01:26:13 -0300

    chore: Remove Tailwind-Merge

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,8 +1,3 @@
-import { clsx, type ClassValue } from 'clsx'
-import { twMerge } from 'tailwind-merge'
-
-export const cn = (...args: ClassValue[]) => twMerge(clsx(args))
-
 export const wait = (delay = 1000) => {
   return new Promise((resolve) => {
     setTimeout(resolve, delay)
